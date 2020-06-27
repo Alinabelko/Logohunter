14e683e5fc5c3628bae43aa6432d71452857903c
Alina <alinabelko@gmail.com>
2020-06-27 18:14:58 +0400

    Initial commit

---
+++ Logohunter.cs
@@ -0,0 +1,36 @@
+﻿using Alturos.Yolo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logohunter_cshap
+{
+    class Logohunter
+    {
+        private YoloWrapper _yolowrapper;
+
+        public Logohunter()
+        {
+            _yolowrapper = new YoloWrapper("yolo_logohunter.cfg",
+                "yolo_logohunter.weights",
+                "yolo_logohunter.names");
+        }
+        public void RunDetection(List<string> imagePaths, List<string> brands = null)
+        {
+            foreach (string imagePath in imagePaths)
+            {
+                var logos = _yolowrapper.Detect(imagePath);
+                //int i = 0;
+                foreach (var logo in logos)
+                {
+                    Bitmap bmp = ImageUtil.CropImage(new Bitmap(imagePath), logo.X, logo.Y, logo.Width, logo.Height);
+                    //save for testing
+                    //bmp.Save($"logo{i}.jpg");
+                    //i++;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Logohunter_cshap
+namespace Logohunter_charp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Logohunter logohunter = new Logohunter();
+
+            List<string> images = new List<string> { "test.jpg" };
+
+            logohunter.RunDetection(images);
         }
     }
 }
